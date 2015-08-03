e3a5ad4f54143408db445b72f3c092416cafeb0b
MaxMeira <maxdawis@gmail.com>
2015-08-02 22:48:41 -0300

    Thrurth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :ideas
 
-  root 'ideas_controller#index'
+  root 'ideas#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
